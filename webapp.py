1f4e5d8044ab37a33fca657e1ad08805c6e16a8f
Kent Wills <rkwills@yelp.com>
2017-02-18 13:27:54 -0800

    only import conftest in dev

--- webapp.py
+++ webapp.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+
 from flask import Flask
 from flask import render_template
 
-from tests.conftest import create_dev_data
 from yelp_beans.routes.api.v1.meeting_requests import meeting_requests
 from yelp_beans.routes.api.v1.metrics import metrics_blueprint
 from yelp_beans.routes.api.v1.preferences import preferences_blueprint
@@ -46,4 +47,7 @@ app.register_blueprint(metrics_blueprint, url_prefix='/v1/metrics')
 app.register_blueprint(preferences_blueprint, url_prefix='/v1/user/preferences')
 app.register_blueprint(user_blueprint, url_prefix='/v1/user')
 
-create_dev_data()
+
+if 'Development' in os.environ.get('SERVER_SOFTWARE', ''):
+    from tests.conftest import create_dev_data
+    create_dev_data()
