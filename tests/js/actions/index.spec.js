d016a5fe7fb1f87d208f1535e35d623f3f34a77e
Aditya Vohra <avohra@yelp.com>
2017-03-09 17:23:12 -0800

    Run eslint and fix some things

--- tests/js/actions/index.spec.js
+++ tests/js/actions/index.spec.js
@@ -1,25 +1,25 @@
+/* eslint-env mocha */
 import { expect } from '../../test_helper';
-import { FETCH_METRICS, FETCH_PREFS, FETCH_USER } from '../../../js/actions/index';
-import { getMetrics, getPreferences, getUser } from '../../../js/actions/index';
+import { FETCH_METRICS, getMetrics } from '../../../js/actions/index';
 
 
-describe('actions', ()=> {
-    describe('getMetric', ()=> {
-        it('has the correct type', ()=>{
-            const metrics = getMetrics('v1');
-            expect(metrics.type).to.equal(FETCH_METRICS)
-        });
+describe('actions', () => {
+  describe('getMetric', () => {
+    it('has the correct type', () => {
+      const metrics = getMetrics('v1');
+      expect(metrics.type).to.equal(FETCH_METRICS);
     });
-    describe('getPreferences', ()=> {
-        it('has the correct type', ()=>{
-            const metrics = getMetrics('v1');
-            expect(metrics.type).to.equal(FETCH_METRICS)
-        });
+  });
+  describe('getPreferences', () => {
+    it('has the correct type', () => {
+      const metrics = getMetrics('v1');
+      expect(metrics.type).to.equal(FETCH_METRICS);
     });
-    describe('getUser', ()=> {
-        it('has the correct type', ()=>{
-            const metrics = getMetrics('v1');
-            expect(metrics.type).to.equal(FETCH_METRICS)
-        });
+  });
+  describe('getUser', () => {
+    it('has the correct type', () => {
+      const metrics = getMetrics('v1');
+      expect(metrics.type).to.equal(FETCH_METRICS);
     });
+  });
 });
--- tests/js/reducers/preference.spec.js
+++ tests/js/reducers/preference.spec.js
@@ -1,14 +1,15 @@
+/* eslint-env mocha */
 import { expect } from '../../test_helper';
-import PrefReducer from '../../../js/reducers/preferences'
-import { getPrefs, FETCH_PREFS } from '../../../js/actions'
+import PrefReducer from '../../../js/reducers/preferences';
+import { FETCH_PREFS } from '../../../js/actions';
 
 
 describe('PrefReducer', () => {
-    it('handles action of getPrefs', () => {
-        var action = {
-            type: FETCH_PREFS,
-            payload: {data: [{title:'metrics'}]}
-        };
-        expect(PrefReducer([], action)[0]['title']).to.equal('metrics');
-    });
+  it('handles action of getPrefs', () => {
+    const action = {
+      type: FETCH_PREFS,
+      payload: { data: [{ title: 'metrics' }] },
+    };
+    expect(PrefReducer([], action)[0].title).to.equal('metrics');
+  });
 });
