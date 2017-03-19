52c9a3ceafe2d017fe7b24d3a2616fe421dd3d7e
Aditya Vohra <avohra@yelp.com>
2017-03-19 10:49:18 -0700

    Cleanup in Makefile, package.json, & .eslintrc.js.
    - rearranged some make targets
    - renamed webpack-dev-server script, removed unused redux-form
    - removed whitelisted 'document' global

--- tests/js/components/MetricsListItem.spec.jsx
+++ tests/js/components/MetricsListItem.spec.jsx
@@ -5,19 +5,19 @@ import MetricsListItem from '../../../js/components/MetricsListItem';
 
 describe('MetricsListItem', () => {
   it('is rendered without data', () => {
-    const component = renderer.create(<MetricsListItem/>);
+    const component = renderer.create(<MetricsListItem />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 
   it('is rendered with one subscription', () => {
-    const metric = {title: 'weekly', subscribed: 10, meetings: 20};
-    const component = renderer.create(<MetricsListItem metric={metric}/>);
+    const metric = { title: 'weekly', subscribed: 10, meetings: 20 };
+    const component = renderer.create(<MetricsListItem metric={metric} />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 
   it('is rendered with many subscriptions', () => {
-    const metric = {title: 'monthly', subscribed: 2, meetings: 4};
-    const component = renderer.create(<MetricsListItem metric={metric}/>);
+    const metric = { title: 'monthly', subscribed: 2, meetings: 4 };
+    const component = renderer.create(<MetricsListItem metric={metric} />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
