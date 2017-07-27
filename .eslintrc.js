90fd71a37ed1312097079e67447af404b366453e
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-07-27 14:01:40 -0700

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     'extends': 'airbnb',
+    'rules': {
+      'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }],
+    },
     'env': {
         'browser': true
-    }
+    },
 };
