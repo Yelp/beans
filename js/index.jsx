81ba4983e0b1f0773b1e4da2d9a1cbf93fce7b73
Kent Wills <rkwills@yelp.com>
2017-11-16 18:12:26 -0800

    bump react deps

--- js/index.jsx
+++ js/index.jsx
@@ -3,16 +3,34 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import { Router, browserHistory } from 'react-router';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 import reducers from './reducers';
-import routes from './routes';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const store = applyMiddleware(ReduxPromise)(createStore);
+import MetricsList from './containers/MetricsList';
+import MeetingRequest from './containers/MeetingRequest';
+import User from './containers/User';
+
+const store = applyMiddleware(ReduxPromise)(createStore)(reducers);
 
 ReactDOM.render(
-  <Provider store={store(reducers)}>
-    <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={User} />
+        <Route path="/dashboard" component={MetricsList} />
+        <Route path="/user/:email" component={User} />
+        <Route path="/meeting_request/:id" component={MeetingRequest} />
+        <Footer />
+      </div>
+    </Router>
   </Provider>,
   document.querySelector('#container'),
 );
+
