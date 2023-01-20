7ee92c97c1e15c3069ef58ccf56902305a1ada09
I Chen Kao <ichenkao@c02f60luml87.lan>
2023-01-20 15:14:48 +0000

    UPT: added user distance algorithm.

--- api/yelp_beans/matching/match_utils.py
+++ api/yelp_beans/matching/match_utils.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 from datetime import datetime
 from datetime import timedelta
 
+import networkx as nx
+import pandas as pd
+import requests
 from database import db
 from yelp_beans.logic.config import get_config
 from yelp_beans.models import Meeting
@@ -10,6 +13,9 @@ from yelp_beans.models import MeetingParticipant
 from yelp_beans.models import MeetingSpec
 from yelp_beans.models import User
 
+
+CORP_API_TOKEN = 'xxxx'
+CORP_API = 'https://corpapi.yelpcorp.com/v1'
 
 def save_meetings(matches, spec):
     for match in matches:
@@ -88,3 +94,83 @@ def get_previous_meetings(subscription, cooldown=None):
     disallowed_meetings = {tuple([meeting.id for meeting in meeting]) for meeting in disallowed_meetings}
 
     return disallowed_meetings
+
+def jaccard(list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    if intersection == 0:
+        return 1
+    else:
+        union = (len(list1) + len(list2)) - intersection
+        return float(intersection) / union
+
+def get_pairwise_distance(user_pair, org_graph, employee_df, max_tenure=1000,):
+    """
+    get the distance between two users.
+    The returned distance score is a linear combination of the multiple user attributes' distnace (normalized).
+    The importance of each attribute is considered equal.
+    User attribute considered:
+    1. team/function: distance in the org chart
+    2. location - country, city
+    3. tenure at Yelp
+    4. language
+
+    note: we considered using education and work experience, but think it likely correlates with the first attribute
+    """
+    user_a, user_b = user_pair
+    user_a_attributes = dict(employee_df.loc[user_a])
+    user_b_attributes = dict(employee_df.loc[user_b])
+
+    distance = 0
+
+    # org chart distance
+    dist_1 = nx.shortest_path_length(org_graph, user_a, user_b)
+    dist_1 = dist_1/10  # approx. min-max scaled
+    distance += dist_1
+
+    # location
+    user_a_city, user_a_country = user_a_attributes["Location"].split(", ")
+    user_b_city, user_b_country = user_b_attributes["Location"].split(", ")
+    country_dist = 0 if user_a_country == user_b_country else 1
+    city_dist = 0 if user_a_city == user_b_city else 1
+    dist_2 = country_dist + city_dist
+    dist_2 = dist_2/2  # min-max scaled
+    distance += dist_2
+
+    # tenure
+    dist_3 = abs(int(user_a_attributes["Days_Since_Start"])-int(user_b_attributes["Days_Since_Start"]))
+    dist_3 = dist_3/max_tenure
+    distance += dist_3
+
+    # language
+    lang_similarity = jaccard(user_a_attributes["languages"], user_b_attributes["languages"])
+    dist_4 = 1 - lang_similarity
+    distance += dist_4
+
+    return distance
+
+def get_meeting_weights(allowed_meetings):
+    """
+    generate distance score for each user pairs.
+    """
+    meeting_to_weight = {}
+
+    # fetching employee information and create a pandas dataframe with it
+    employees = pd.DataFrame(requests.get(
+        f'{CORP_API}/employees',
+        headers={'X-API-Key': CORP_API_TOKEN},
+    ).json())
+
+    employees = employees.set_index("Employee_ID")
+    employees = employees[["Manager_ID", "Cost_Center_-_Name", "Days_Since_Start", "Location", "languages",
+                           "Education", "Work_Experience_group", "Pronoun"]]
+    max_tenure = max(employees['Days_Since_Start'].astype(int))
+
+    # yelp employee network graph created through reporting line
+    G = nx.Graph()
+    G.add_edges_from(list(zip(employees.index, employees['Manager_ID'])))
+
+    for user_pair in allowed_meetings:
+        users_distance_score = get_pairwise_distance(user_pair, max_tenure, org_graph=G, employee_df=employees)
+        meeting_to_weight[user_pair] = users_distance_score
+
+    return meeting_to_weight
