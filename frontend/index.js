1e3c07a270f7dbdd8dff8dd22eb92d559a759a28
Kyle Deal <kdeal@kyledeal.com>
2023-09-08 20:28:37 +0000

    Use recommended way to create routes with react-router
    There was an issue where you would sometimes get shown the wrong page
    when loading a url. Hopefully updating to the recommended way of
    defining your routes should fix this.

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,24 +11,38 @@ import User from "./containers/User";
 import SubscriptionsList from "./containers/SubscriptionsList";
 import Subscription from "./containers/Subscription";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <User />,
+  },
+  {
+    path: "/dashboard",
+    element: <MetricsList />,
+  },
+  {
+    path: "/user/:email",
+    element: <User />,
+  },
+  {
+    path: "/meeting_request/:id",
+    element: <MeetingRequest />,
+  },
+  {
+    path: "/admin/subscriptions/:id",
+    element: <Subscription />,
+  },
+  {
+    path: "/admin/subscriptions",
+    element: <SubscriptionsList />,
+  },
+]);
+
 const root = createRoot(document.querySelector("#container"));
 root.render(
-  <Router>
-    <div>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<User />} />
-        <Route path="/dashboard" element={<MetricsList />} />
-        <Route path="/user/:email" element={<User />} />
-        <Route path="/meeting_request/:id" element={<MeetingRequest />} />
-        <Route path="/admin/subscriptions/:id" element={<Subscription />} />
-        <Route
-          exact
-          path="/admin/subscriptions"
-          element={<SubscriptionsList />}
-        />
-      </Routes>
-      <Footer />
-    </div>
-  </Router>,
+  <div>
+    <Header />
+    <RouterProvider router={router} />
+    <Footer />
+  </div>,
 );
